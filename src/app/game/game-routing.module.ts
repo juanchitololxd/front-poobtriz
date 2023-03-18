4e8242b85e3a53dd3b8e6408c22e5206b3b10342
xLoro <cardenas.santiago@hotmail.com>
2023-03-17 19:07:07 -0500

    HU-RVDrontEndCorrecion

--- src/app/game/game-routing.module.ts
+++ src/app/game/game-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
-
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   imports: [  RouterModule.forChild([
-    { path: '', component: GameComponent }
+    { path: '', component: GameComponent },
+    { path: 'board', component: BoardComponent }
   ])],
 
 
