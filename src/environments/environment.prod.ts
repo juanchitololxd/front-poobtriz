e3fcc2f91d62fa6ef0a6a1ff969d461a1342f7ed
Juan Fonseca <juanpablofonsecac5@gmail.com>
2023-12-06 19:48:13 -0500

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  gateway: "http://ec2-54-90-230-106.compute-1.amazonaws.com:8080/",
-  lobby: "http://ec2-54-90-80-176.compute-1.amazonaws.com:8082/"
+  gateway: "http://ec2-54-147-46-87.compute-1.amazonaws.com:8080/",
+  lobby: "http://ec2-54-236-23-42.compute-1.amazonaws.com:8082/"
 };
